7d04e12e86e64e4ca594615fc03df8fc2d3e4e98
brooke <brooke.mitchell@comcast.net>
2018-11-02 16:24:33 -0400

    fixed initial bugs

--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -10,25 +10,18 @@ module.exports = function(app) {
     // get the data from the user
     app.get("/api/friends", function(req,res) {
         res.json(arrayOfFriends)
+        console.log(arrayOfFriends);
     });
     
-    // app.get("/api/survey", function(req,res) {
-    //     res.json(surveyData)
-    // });
+
     // API POST Requests
-    // handles when user submits a form and submits data to the server
-    // When a user submits form data (a JSON objects)
-    // the JSON is pushed to javascript array
-    // user fills out friend survey and the data gets sent to the server
-    // the server saves the data to the friendData array
-    
-    
+
       app.post("/api/friends", function(req, res) {
         // console.log(req.body.scores);
         // console.log(thisFriend);
     
-        // Receive user details (name, photo, scores)
         var thisFriend = req.body;
+
         var userScores = thisFriend.scores;
 
         var bestFriendName = '';
@@ -37,23 +30,28 @@ module.exports = function(app) {
 
         for(var i = 0; i < arrayOfFriends.length; i++) {
             var difference = 0;
+            for(var j = 0; j < userScores.length; j++) {
+              userScores[j] = parseInt(userScores[j]);
+              difference += Math.abs(arrayOfFriends[i].scores[j] - userScores[j]);
+            }
+
+            console.log(difference);
             
             
          // in this for-loop, start off with a zero difference and compare the user and the ith friend scores, one set at a time
              //  whatever the difference is, add to the total difference
             
-             // parseInt for scores
-        //   thisFriend.scores[i] = parseInt(thisFriend.scores[i]);
+          // parseInt for scores
 
-          for(var j = 0; j < userScores.length; j++) {
-            difference += Math.abs(arrayOfFriends[i].scores[j] - userScores[j]);
-          }
          
           // if there is a new minimum, change the best friend index and set the new minimum for next iteration comparisons
           if(difference < totalDifference) {
             totalDifference = difference;
-            bestFriendName = arrayOfFriends[i].name;
-            bestFriendPic = arrayOfFriends[i].photo;
+            bestFriendName = arrayOfFriends[i].friendName;
+            bestFriendPic = arrayOfFriends[i].friendImage;
+
+            console.log(bestFriendName);
+
           }
        
         }
@@ -64,7 +62,7 @@ module.exports = function(app) {
     
 	// Send appropriate response
     res.json({status: 'OK', bestFriendName: bestFriendName, bestFriendPic: bestFriendPic});
-   
+    
     });
 };
    
